23df2e5175ac58b8766c021bbcf15c29d3b97e9a
jjreedv <87283488+jjreedv@users.noreply.github.com>
2021-10-05 09:49:52 -1000

    Proxy Contract
    Contract Upgrade

---
+++ scripts/deploy_and_upgrade.py
@@ -0,0 +1,39 @@
+from scripts.help_script import get_account, encode_function_data, upgrade
+from brownie import network, Box, ProxyAdmin, TransparentUpgradeableProxy, Contract, BoxV2
+
+
+def main():
+    account = get_account()
+    print(f"Deploying to {network.show_active()}")
+    run = Box.deploy({"from": account})
+
+    proxy_admin = ProxyAdmin.deploy({"from": account})
+
+    # initializer == constructor for proxies
+    # initializer = run.store, 1
+    # box_encoded_initializer_function = encode_function_data(initializer)
+
+    box_encoded_initializer_function = encode_function_data()
+
+    # deploy on proxy
+    proxy = TransparentUpgradeableProxy.deploy(
+        run.address, proxy_admin.address, box_encoded_initializer_function, {
+            "from": account, "gas_limit": 1000000}
+    )
+    print(f"Proxy deployed to {proxy}")
+
+    # call proxy not box || proxy points to box
+    proxy_box = Contract.from_abi("Box", proxy.address, Box.abi)
+    proxy_box.store(1, {"from": account})
+    print(proxy_box.retrieve())
+
+    box_v2 = BoxV2.deploy({"from": account})
+    upgrade_txn = upgrade(account, proxy, box_v2.address,
+                          proxy_admin_contract=proxy_admin)
+    upgrade_txn.wait(1)
+
+    # redefine for v2
+    proxy_box = Contract.from_abi("BoxV2", proxy.address, BoxV2.abi)
+    # call new function
+    proxy_box.increment({"from": account})
+    print(proxy_box.retrieve())
---
+++ scripts/help_script.py
@@ -0,0 +1,59 @@
+from brownie import accounts, network, config
+import eth_utils
+
+LOCAL_BLOCKCHAIN_ENVIROMENTS = ["hardhat",
+                                "development", "ganache", "mainnet-fork"]
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIROMENTS:
+        return accounts[0]
+    if id:
+        return accounts.load[0]
+    return accounts.add(config["wallets"]["from_key"])
+
+
+def encode_function_data(initializer=None, *args):
+    if len(args) == 0 or not initializer:
+        return eth_utils.to_bytes(hexstr="0x")
+    return initializer.encode_input(*args)
+
+
+transaction = None
+
+
+def upgrade(
+    account,
+    proxy,
+    newimplementation_address,
+    proxy_admin_contract=None,
+    initializer=None,
+    *args
+):
+    transaction = None
+    if proxy_admin_contract:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            transaction = proxy_admin_contract.upgradeAndCall(
+                proxy.address,
+                newimplementation_address,
+                encoded_function_call,
+                {"from": account},
+            )
+        else:
+            transaction = proxy_admin_contract.upgrade(
+                proxy.address, newimplementation_address, {"from": account}
+            )
+    else:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            transaction = proxy.upgradeToAndCall(
+                newimplementation_address, encoded_function_call, {
+                    "from": account}
+            )
+        else:
+            transaction = proxy.upgradeTo(
+                newimplementation_address, {"from": account})
+    return transaction
